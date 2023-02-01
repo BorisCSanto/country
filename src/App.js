d90fe355cffb370270a60b2082d75ce1154457e0
BorisCSanto <contact@chrismantcreation.fr>
2023-02-01 21:53:06 +0100

    react installé et nav en place

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Logo from './components/Logo';
+import Navigation from './components/Navigation';
+import About from './pages/About';
+import Home from './pages/Home';
+import "./styles/style.css";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Logo />
+      <Navigation />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+
+        {/* default */}
+        <Route path="*" element={<Home />} />
+      </Routes>
+
+    </BrowserRouter>
+
+
   );
-}
+};
 
 export default App;
---
+++ src/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div>
+            <h1>A propos</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam vel officia deserunt ipsam culpa, obcaecati dicta nisi. Dicta accusamus eveniet ut cupiditate non voluptatum ipsam. Magni quas temporibus molestiae sed.</p>
+        </div>
+    );
+};
+
+export default About;
